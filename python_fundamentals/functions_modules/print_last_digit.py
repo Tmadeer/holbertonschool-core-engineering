448af135693930a7df19ff21d8f13a176737da46
Tmadeer <imorganista_@hotmail.com>
2026-09-18 19:58:52 +0300

    Fix print_last_digit newline issue

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 def print_last_digit(number):
-    """Print and return the last digit of number (always positive)."""
     digit = abs(number) % 10
-    print(digit)
+    print(digit, end="")
     return digit
