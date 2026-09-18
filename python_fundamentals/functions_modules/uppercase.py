beb158164ac068ca9508338ccfe9506d84da83b4
Tmadeer <imorganista_@hotmail.com>
2026-09-18 19:44:05 +0300

    Fix uppercase.py to use format() and handle non-string input

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 def uppercase(str):
-    """Print str converted to uppercase, followed by a newline."""
     result = ""
-    for c in str:
+    for c in "{}".format(str):
         if ord('a') <= ord(c) <= ord('z'):
-            result = result + chr(ord(c) - 32)
+            result = "{}{}".format(result, chr(ord(c) - 32))
         else:
-            result = result + c
-    print(result)
+            result = "{}{}".format(result, c)
+    print("{}".format(result))
