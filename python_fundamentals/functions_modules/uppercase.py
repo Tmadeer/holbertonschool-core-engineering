620ec9e3c3261a7c30ff118bb7cdc87c09d316b3
Tmadeer <imorganista_@hotmail.com>
2026-09-18 19:47:36 +0300

    Add uppercase.py with ASCII conversion and TypeError on non-string input

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 def uppercase(str):
+    """Print str converted to uppercase, followed by a newline."""
     result = ""
-    for c in "{}".format(str):
+    for c in str:
         if ord('a') <= ord(c) <= ord('z'):
             result = "{}{}".format(result, chr(ord(c) - 32))
         else:
