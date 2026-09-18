644898b25ca6ff37eb2ea41de7ebec8a7477da78
Tmadeer <imorganista_@hotmail.com>
2026-09-18 22:09:13 +0300

    Add simple_add.py with main execution guard

---
+++ python_fundamentals/functions_modules/simple_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module that provides a simple add function."""
+
+
+def add(a, b):
+    """Returns the sum of two numbers."""
+    return a + b
+
+
+if __name__ == "__main__":
+    print(add(3, 5))
